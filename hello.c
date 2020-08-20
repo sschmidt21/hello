4c2464b66f1338e9482969db98cab48efd8dcceb
sschmidt21 <sschmidt21@huskers.unl.edu>
2020-08-19 21:47:42 -0500

    Added Majors
    Added a line to print my majors and removed vertical whitespace

--- hello.c
+++ hello.c
@@ -2,16 +2,13 @@
  * Author: Spencer Schmidt
  *         sschmidt21@huskers.unl.edu
  * Date: 2020/08/17
- *
  * A simple hello world program in C
- *
  */
 #include <stdlib.h>
 #include <stdio.h>
 
 int main(int argc, char **argv) {
-
   printf("Spencer Schmidt\n");
-
+  printf("Geology and Mathematics\n");
   return 0;
 }
