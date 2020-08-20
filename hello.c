7ac05ce82d3360ea43a2877f60f53db0b0474694
sschmidt21 <sschmidt21@huskers.unl.edu>
2020-08-19 21:36:26 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Spencer Schmidt
+ *         sschmidt21@huskers.unl.edu
+ * Date: 2020/08/17
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Spencer Schmidt\n");
+
+  return 0;
+}
